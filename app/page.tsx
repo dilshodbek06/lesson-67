4bfe3b32db39a53f327efc108d2c33b1a71c8a7f
Dilshodbek <ziyodullayevdilshod7@gmail.com>
2025-04-07 11:17:37 +0500

    gdf

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ const HomePage = () => {
       <h1>Hello world</h1>
       <h2>Hello from teacher</h2>
       <h3>Hello from teacher 2</h3>
+      <h3>Azizbek </h3>
+      <h4>Dava</h4>
+      <h5>Bugun sening kuning</h5>
+      <b>BU bu tegi</b>
     </div>
   );
 };
